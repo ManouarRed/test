dd9cebe63a687dc1d1d4e8964a9f99aeb2865575
ManouarRed <139481088+ManouarRed@users.noreply.github.com>
2025-05-23 14:30:30 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(name)
+
+@app.route('/')
+def hello():
+return 'Hello, World!'
+
+if name == 'main':
+# Listen on all interfaces, port 8000
+app.run(host='0.0.0.0', port=8000)
